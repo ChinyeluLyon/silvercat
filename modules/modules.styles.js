2489a52a8209d486b0fed895ba59b989f86233dc
Chinyelu Lyon <chinyelulyon@gmail.com>
2022-10-17 22:09:19 +0100

    add styles

---
+++ modules/modules.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const TransactionWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 10px;
+  padding: 25px;
+  box-shadow: 1px 5px 10px #c8c8c8;
+  border-radius: 10px;
+`;
--- modules/transactions.js
+++ modules/transactions.js
@@ -1,20 +1,19 @@
 import React from "react";
+import * as S from "./modules.styles";
 
 const Transactions = ({ transactionArray }) => {
   return transactionArray?.map((t, idx) => {
     const date = new Date(t.createdAt).toDateString();
     return (
-      <div key={idx}>
+      <S.TransactionWrapper key={idx}>
         <div>
-          <div>
-            <b>Amount: </b>
-            <p>£{t.amount}</p>
-          </div>
-          <div>
-            <p>{date}</p>
-          </div>
+          <b>Amount: </b>
+          <p>£{t.amount}</p>
         </div>
-      </div>
+        <div>
+          <p>{date}</p>
+        </div>
+      </S.TransactionWrapper>
     );
   });
 };
